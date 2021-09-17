e6a723c51fe903fcf494f0ac8c94173bfe53ac1c
Kyohei Tanoue <kyohey.tan@gmail.com>
2021-09-17 18:06:03 +0900

    なし

--- nush/client/public/js/payment.js
+++ nush/client/public/js/payment.js
@@ -43,4 +43,124 @@ const submitButton = document.getElementById("payment-form-submit");
 // ボタンがクリックされたら、アクションを実行
 submitButton.addEventListener("click", function (event) {
 
+    displaySpinner();
+
+    stripe
+        .createPaymentMethod("card", card) // <== ここでPromiseが返ってくるので、thenで処理を続ける
+        .then(function (result) {
+            if (result.error) {
+                onError();// エラー時の処理
+            } else {
+                // 成功した時の処理。サーバサイドに注文情報を送信する
+                order.paymentMethodId = result.paymentMethod.id;
+                // サーバサイドへ決済情報を渡して結果をハンドリングする
+                // サーバは http://localhost:3000/v1/order/payment にPOSTでリクエストを受け付けている
+                fetch("http://localhost:3000/v1/order/payment", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify(order) })
+                    .then(function (result) {
+                        return result.json();  // <== HTTPレスポンスからボディをJSONを取り出して次のメソッドに引き渡す
+                    })
+                    .then(function (response) {
+                        onComplete(response);// 正常終了。
+                    });
+            }
+        })
+        .catch(function () {
+            onError();// エラー発生時。
+        });
 });
+
+let returnButtonNormal = document.getElementById("return-button-normal");
+let returnButtonError = document.getElementById("return-button-error");
+let returnButtonNotYet = document.getElementById("return-button-not-yet");
+let returnButtonDefault = document.getElementById("return-button-default");
+
+returnButtonNormal.addEventListener("click", reset);
+returnButtonError.addEventListener("click", reset);
+returnButtonNotYet.addEventListener("click", reset);
+returnButtonDefault.addEventListener("click", reset);
+
+function reset(event) {
+    hideError();
+    hideMessage();
+    hideNotYetMessage();
+    displayButton();
+
+    card.mount("#card-element");
+}
+
+function onComplete(response) {
+
+    shutdown();
+
+    // スピナー終了
+    hideSpinner();
+
+    if (response.error) {
+        onError();
+    } else if (response.paymentIntentStatus === "succeeded") {
+        // 確定ボタンを消して完了メッセージを表示
+        displayMessage();
+    } else {
+        displayNotYetMessage();
+    }
+}
+
+function onError() {
+
+    shutdown();
+
+    if (!document.querySelector(".spinner-border").classList.contains("collapse")) {
+        hideSpinner();
+    }
+    // エラーメッセージを表示
+    displayError();
+
+}
+
+function shutdown() {
+    card.unmount();
+    hideButton();
+}
+
+function hideSpinner() {
+    document.querySelector(".spinner-border").classList.add("collapse")
+}
+
+function displaySpinner() {
+    document.querySelector(".spinner-border").classList.remove("collapse");
+}
+
+// エラーメッセージ
+function hideError() {
+    document.querySelector(".contents-payment-error").classList.add("collapse");
+}
+
+function displayError() {
+    document.querySelector(".contents-payment-error").classList.remove("collapse");
+}
+
+// 成功メッセージ
+function displayMessage() {
+    document.querySelector(".contents-payment-result").classList.remove("collapse");
+}
+
+function hideMessage() {
+    document.querySelector(".contents-payment-result").classList.add("collapse");
+}
+
+function displayNotYetMessage() {
+    document.querySelector(".contents-payment-not-yet").classList.remove("collapse");
+}
+
+function hideNotYetMessage() {
+    document.querySelector(".contents-payment-not-yet").classList.add("collapse");
+}
+
+// 注文確定ボタン
+function hideButton() {
+    document.querySelector("#payment-form-submit").classList.add("collapse");
+}
+
+function displayButton() {
+    document.querySelector("#payment-form-submit").classList.remove("collapse");
+}
